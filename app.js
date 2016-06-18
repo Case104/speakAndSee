d85a53adca3305d3d8cf9268b375d6eb9d8dee47
Case104 <jpcase104@gmail.com>
2016-06-18 18:25:13 -0400

    Adding flickr responses. MVP complete

--- app.js
+++ app.js
@@ -11,9 +11,16 @@ $('#start-search').click(function(){
 	recognition.onresult = function(event){
 		recognition.stop();
 		$('#start-search').html('Search again!');
-		speechQuery = event.results[0][0].transcript;
+		var speechQuery = event.results[0][0].transcript;
 
+		$.ajax({
+			url: 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + apiKey + '&format=json&nojsoncallback=1&text=' + speechQuery +'&extras=url_m&per_page=10',
+			dataType: 'json'
+		}).then(function(response){
+			response.photos.photo.forEach(function(photo){
+				$('#search-results').prepend("<div class='img-result'><img src='" + photo.url_m + "'</div>");
+			});
+		});
 		
-	}
-
-})
+	};
+});
